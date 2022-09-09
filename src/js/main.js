172ddcb258a0465a9d181af2470f6d52f5e2775f
ola123-creator <arsjolahalili@gmail.com>
2022-09-09 03:12:37 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+var countDownDate = new Date("Sep 15, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = " 0 "+ days + "  :  " + hours + "  :  "
+  + minutes + "  :  " + seconds ;
+
+ 
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1400: {
+
+          slidesPerView: 3 ,
+      },
+      640: {
+          slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 1,
+        
+    },
+      320: {
+          slidesPerView: 1,
+          
+      }
+  }
+  });
